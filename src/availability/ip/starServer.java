1224f6633d83496ec615033883c85f24ae86f195
krisipironkova <krisi.pironkova@gmail.com>
2015-11-16 16:34:19 +0200

    Added startServer function

--- src/availability/ip/starServer.java
+++ src/availability/ip/starServer.java
@@ -1,9 +1,11 @@
 package availability.ip;
 
+import java.io.IOException;
+
 public class starServer {
 
 	private static final int SERVER_PORT = 31111;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		final Server server = new Server(SERVER_PORT);
 		server.startServer();
 	}
