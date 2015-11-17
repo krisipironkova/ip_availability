4a792a32ea4095ad3242e1bdac74d465ddc209c7
krisipironkova <krisi.pironkova@gmail.com>
2015-11-17 04:17:09 +0200

    Login and logout functions + getters for name and login counts. IsLogged check and user info function.

--- src/availability/ip/User.java
+++ src/availability/ip/User.java
@@ -3,6 +3,7 @@ package availability.ip;
 import java.util.List;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
 
 	public class User {
 		private String username;
@@ -16,5 +17,49 @@ import java.util.ArrayList;
 			this.loginCount = 0;
 			this.logged = false;
 			this.socket = socket;
+		}
+		public Socket getSocket() {
+			return socket;
+		}
+		
+		public String login() {
+			if(!isLogged()) {	
+				this.logged = true;
+				this.loginCount++;
+				this.loginHistory.add(new Interval(new Date()));
+				return "ok";
+			} return "error:alreadyloggedin";
+		}
+		
+		public String logout() {
+			if(isLogged()) {
+				this.logged = false;
+				this.loginHistory.get(this.loginHistory.size() - 1).setTo(new Date());
+				return "ok";
+			} return "error:alreadyloggedout";
+		}
+		
+		public String getName() {
+			return this.username;
+		}
+		
+		public int getLoginCounts() {
+			return this.loginCount;
+		}
+		
+		public boolean isLogged() {
+			return this.logged;
+		}
+		
+		public String info() {
+			String info =  "ok:" + getName() + ":" + isLogged() + ":" + getLoginCounts();
+			
+			for (Interval temp_list: this.loginHistory) {
+				info += ":" + temp_list.from();
+				
+				if (temp_list.to() != null) info += ":" + temp_list.to();
+			}
+			
+			return info;
 		}
 	}
