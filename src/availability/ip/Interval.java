c02ff74ca15881ef1d21f3443c90b45fc8054d46
krisipironkova <krisi.pironkova@gmail.com>
2015-11-17 04:38:20 +0200

    Fullfilling the Interval class

--- src/availability/ip/Interval.java
+++ src/availability/ip/Interval.java
@@ -1,5 +1,29 @@
 package availability.ip;
 
-public class Interval {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Interval {
+	private Date from, to;
+	
+	public Interval(Date from) {
+		this.from = from;
+	}
+	
+	public void setTo(Date to) {
+		this.to = to;
+	}
+
+	private String formatDate(Date date) {
+		return new SimpleDateFormat("yyyy-­MM-dd'T'HH'_'mm'_'ss.SSSZ").format(date);
+	}
+	
+	public String from() {
+		return this.formatDate(this.from);
+	}
+	
+	public String to() {
+		return this.to != null ? this.formatDate(this.to) : null;
+	}
+	
 }
