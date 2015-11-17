867a5dad66f887b8affb1537516d414a669d5af2
krisipironkova <krisi.pironkova@gmail.com>
2015-11-17 04:56:39 +0200

    startServer and setRunning fuctions

--- src/availability/ip/Server.java
+++ src/availability/ip/Server.java
@@ -1,13 +1,43 @@
 package availability.ip;
 
-public class Server {
-	private final int port;
 
-	 public Server(int port) {
-		 this.port = port;
-	 }
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Server {
+	private boolean running;
+	private final int port;
+	private ServerSocket serverSocket;
+	private final List<ClientHandler> clients = Collections.synchronizedList(new LinkedList<ClientHandler>());
 	
-	 public void startServer() {
+	public Server(int port) {
+		this.port = port;
+	}
 	
-	 }
+	public void startServer() throws IOException {
+		setRunning();
+		this.serverSocket = new ServerSocket(port);
+		while(isRunning()) {
+			final Socket socket = serverSocket.accept();
+			final ClientHandler client = new ClientHandler(socket, this);
+			new Thread(client).start();
+		}
+		serverSocket.close();
+	}
+	
+	
+	public synchronized boolean isRunning() {
+		return running;
+	}
+	
+	private synchronized void setRunning() {
+		if(running) {
+			throw new IllegalStateException("Already running");
+		}
+		running = true;
+	}
 }
